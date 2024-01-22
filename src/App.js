f7a54bbcf227603425896a40cbf9d38487f582af
Firdosh01 <firdosh7392@gmail.com>
2024-01-22 06:55:59 -0500

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './page/Home';
+import About from './page/About';
+import SignIn from './page/SignIn';
+import SignUp from './page/SignUp';
+import Dashboard from './page/Dashboard';
+import Projects from './page/Projects';
 
 function App() {
   return (
     <div>
-      <h1>Let's start</h1>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
     </div>
   );
 }
