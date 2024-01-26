8cd1406fd20400365a2cdcf70bc3562f1800fe34
Arzaid-aka-Rz <arzaid010103@gmail.com>
2024-01-26 23:19:25 +0530

    add redux persist

--- src/index.js
+++ src/index.js
@@ -4,17 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import { store } from './redux/store.js';
+import { store,persistor  } from './redux/store.js';
 import { Provider } from 'react-redux';
+import { Toaster } from 'react-hot-toast';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <PersistGate persistor={persistor}>
   <Provider store={store}>
     <BrowserRouter>
         <App />
         <Toaster />
     </BrowserRouter>
   </Provider>
+  </PersistGate>
 );
 
 // If you want to start measuring performance in your app, pass a function
