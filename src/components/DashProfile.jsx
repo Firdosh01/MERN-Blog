1df1ee3796d36c08078350b0d79b0ae8bf60306f
Firdosh01 <firdosh7392@gmail.com>
2024-01-30 10:40:36 -0500

    implement profile picture functionality

--- src/components/DashProfile.jsx
+++ src/components/DashProfile.jsx
@@ -1,16 +1,77 @@
 import { Button, TextInput } from "flowbite-react";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+// import getStorage from "redux-persist/es/storage/getStorage";
+import { app } from '../firebase'
+import {getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
 
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
+  const [imageFile, setImageFile] = useState(null)
+  const [imageFileUrl, setImageFileUrl] = useState(null)
+  const filePictureRef = useRef()
+  const [imgFileUploadProgress, setImgFileUploadProgress] = useState(0)
+  const [imgFileUploadError, setImgFileUploadError] = useState(null)
+  console.log(imgFileUploadProgress, imgFileUploadError )
+
+  const handleImgChange = (e) => {
+    const file = e.target.files[0]
+    if(file){
+      setImageFile(file)
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  }
+  // console.log(imageFile, imageFileUrl)
+
+  useEffect(() => {
+    if(imageFile){
+      uploadImg()
+    }
+  },[imageFile])
+
+  const uploadImg = async () => {
+    // service firebase.storage {
+    //   match /b/{bucket}/o {
+    //     match /{allPaths=**} {
+    //       allow write: if
+    //       request.resource.size < 2 * 1024 * 1024 &&
+    //       request.resource.contentType.matches('image/.*')
+    //     }
+    //   }
+    // }
+    const storage = getStorage(app)
+    const fileName = new Date().getTime() + imageFile.name;
+    const storageRef = ref(storage, fileName)
+    const uploadTask = uploadBytesResumable(storageRef, imageFile)
+    
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress = 
+        (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImgFileUploadProgress(progress.toFixed(0))
+      },
+      (error) => {
+        setImgFileUploadError('Could not upload image (File must be less then 2MB)')
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+
+          setImageFileUrl(downloadURL)
+        })
+      }
+    )
+    
+  }
+
   return (
     <div className="w-full max-w-lg p-3 mx-auto">
       <h1 className="font-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col gap-4">
-        <div className="self-center w-32 h-32 overflow-hidden shadow-md cursor-pointer">
+        <input type="file" accept="image/*" onChange={handleImgChange} ref={filePictureRef} className="hidden" />
+        <div className="self-center w-32 h-32 overflow-hidden shadow-md cursor-pointer" onClick={() => filePictureRef.current.click()}>
           <img
-            src={currentUser.profilePicture}
+            src={imageFileUrl || currentUser.profilePicture}
             alt="user"
             className="w-full h-full border-4 rounded-full border-[lightGray] object-cover select-none"
           />
